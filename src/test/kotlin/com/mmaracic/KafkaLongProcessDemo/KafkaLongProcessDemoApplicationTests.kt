6bb2dbfce1c9e7a5e0a0224a88e0e1275aab8f9c
mmaracic <marijo.maracic@gmail.com>
2023-09-19 23:17:46 +0200

    Initial commit

---
+++ src/test/kotlin/com/mmaracic/KafkaLongProcessDemo/KafkaLongProcessDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mmaracic.KafkaLongProcessDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaLongProcessDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
