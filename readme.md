bf97290177c9deddc0f0a2eb878de756680502b2
mmaracic <marijo.maracic@gmail.com>
2023-09-24 03:12:12 +0200

    Created fully functional generic and non-generic consumers, both batched and single.

--- readme.md
+++ readme.md
@@ -2,16 +2,26 @@
 
 Project to Demo Kafka usage for long running processes.
 
+Long running process
 https://medium.com/codex/dealing-with-long-running-jobs-using-apache-kafka-192f053e1691
 
+Using Avro schemas (not used)
 https://www.baeldung.com/spring-cloud-stream-kafka-avro-confluent
 
+Spring Cloud Stream
+https://docs.spring.io/spring-cloud-stream/docs/current/reference/html/spring-cloud-stream.html#_configuration_options
+
+Spring cloud kafka binder properties
 https://cloud.spring.io/spring-cloud-stream-binder-kafka/spring-cloud-stream-binder-kafka.html#pause-resume
 
+Writing consumers and producers in newer versions of spring cloud stream
 https://stackoverflow.com/questions/65978055/enablebinding-output-input-deprecated-since-version-of-3-1-of-spring-cloud-str
 
-Custom java DNS aliases
+Custom java DNS aliases (not used)
 https://stackoverflow.com/questions/11647629/how-to-configure-hostname-resolution-to-use-a-custom-dns-server-in-java
+
+Hosts file Win:
+c:\Windows\System32\Drivers\etc\hosts
 
 ## Commands
 ```
--- src/main/kotlin/com/mmaracic/kafka_long_process_demo/KafkaLongProcessDemoApplication.kt
+++ src/main/kotlin/com/mmaracic/kafka_long_process_demo/KafkaLongProcessDemoApplication.kt
@@ -1,9 +1,12 @@
 package com.mmaracic.kafka_long_process_demo
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.context.annotation.Bean
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.messaging.support.GenericMessage
 import org.springframework.stereotype.Service
 import java.time.OffsetDateTime
 import java.util.function.Consumer
@@ -20,33 +23,65 @@ class KafkaLongProcessDemoApplication {
 
     private val logger = Logger.getLogger(KafkaLongProcessDemoApplication::class.java.name)
 
-    //consumer
+    //consumer - singe
     @Bean
     fun messageConsumer(): Consumer<Message> {
-        return Consumer { listen(message = it) }
+        return Consumer {
+            logger.info("Received message ${it.dateTime}")
+        }
     }
 
-    fun listen(message: Message) {
-        logger.info("Received message ${message.dateTime} ${message.title}")
-    }
-
-}
-
-//Producer1 - automated
-@Service
-class ProducerAutomated {
+    //consumer - batch
     @Bean
-    fun studentsChannel(): Supplier<Message> {
-        return Supplier { Message("Message sample", OffsetDateTime.now()) }
+    fun messagesConsumer(): Consumer<List<Message>> {
+        return Consumer {
+            logger.info("Received batch message  count ${it.size}")
+            it.forEach {
+                logger.info("Received batched message ${it.dateTime}")
+            }
+        }
+    }
+
+    //Generic consumers are better for debugging errors while setting up, contain message headers with offsets and payload type
+    //generic consumer - singe
+    @Bean
+    fun genericMessageConsumer(): Consumer<GenericMessage<Message>> {
+        return Consumer {
+            logger.info("Received message ${it.payload.dateTime} on offset ${it.headers["kafka_offset"]}")
+        }
+    }
+
+    //generic consumer - batch
+    @Bean
+    fun genericMessagesConsumer(): Consumer<GenericMessage<List<Message>>> {
+        return Consumer {
+            logger.info("Received batch message  count ${it.payload.size}, offsets ${it.headers["kafka_offset"]}")
+            it.payload.forEach {
+                logger.info("Received batched message ${it.dateTime}")
+            }
+        }
+    }
+
+    //Producer1 - automated
+    @Bean
+    fun messageProducerAutomated(): Supplier<Message> {
+        return Supplier { Message(OffsetDateTime.now()) }
     }
 }
 
 //Producer2 - manual
 @Service
 class ProducerManual(private val streamBridge: StreamBridge) {
+
+    private val logger = Logger.getLogger(ProducerManual::class.java.name)
+
     fun publish(message: Message) {
-        streamBridge.send("studentsChannel-out-0", message)
+        logger.info("Producing message: $message")
+        streamBridge.send("messageProducerAutomated-out-0", message)
     }
 }
 
-class Message(val title: String, val dateTime: OffsetDateTime)
+//Required to handle default message type deserialisation which is json. Uses jackson under the hood, used in yml config
+class MessageDeserializer : JsonDeserializer<Message?>()
+
+class Message(@JsonProperty("dateTime") val dateTime: OffsetDateTime)
